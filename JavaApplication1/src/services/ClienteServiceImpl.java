82aef0f8f51c4ff6614acf4c6f0da39bb8f9cb22
Guilherme ZF <guilhermefragiacomo1@gmail.com>
2024-09-02 15:02:49 -0300

    tentei consertar os erros

--- JavaApplication1/src/services/ClienteServiceImpl.java
+++ JavaApplication1/src/services/ClienteServiceImpl.java
@@ -13,52 +13,53 @@ import updaters.UsuarioUpdater;
 
 public class ClienteServiceImpl implements ClienteService {
 
-         private UsuarioRepository repository;
-         private UsuarioUpdater usuarioUpdater;
+    private UsuarioRepository repository;
+    private UsuarioUpdater usuarioUpdater;
 
-         public ClienteServiceImpl(UsuarioRepository repository) {
-                  this.repository = repository;
-                  this.usuarioUpdater = new UsuarioUpdater(repository);
-         }
+    public ClienteServiceImpl(UsuarioRepository repository) {
+        this.repository = repository;
+        this.usuarioUpdater = new UsuarioUpdater(repository);
+    }
 
-         @Override
-         public List<Cliente> buscarTodosClientes() {
-                  return repository.buscarTodosClientes();
-         }
+    @Override
+    public List<Cliente> buscarTodosClientes() {
+        return repository.buscarTodosClientes();
+    }
 
-         @Override
-         public Cliente buscarClientePorLogin(String login) {
-                  if (login != null) {
-                           if (repository.contem(login)) {
-                                    if (repository.buscarPorLogin(login) instanceof Cliente cliente) {
-                                             return cliente;
-                                    }
-                           }
-                  }
-                  throw new LoginInvalidoException("Login de cliente invalido.");
-         }
+    @Override
+    public Cliente buscarClientePorLogin(String login) {
+        if (login != null) {
+            if (repository.contem(login)) {
+                if (repository.buscarPorLogin(login) instanceof Cliente) {
+                    return (Cliente) repository.buscarPorLogin(login);
+                }
+            }
+        }
+        throw new LoginInvalidoException("Login de cliente invalido.");
+    }
 
-         @Override
-         public void salvarCliente(Cliente cliente) {
-                  if (cliente != null) {
-                           if (!repository.contem(cliente.getLogin())) {
-                                    repository.salvar(cliente);
-                           }
-                  }
-         }
+    @Override
+    public void salvarCliente(Cliente cliente) {
+        if (cliente != null) {
+            if (!repository.contem(cliente.getLogin())) {
+                repository.salvar(cliente);
+            }
+        }
+    }
 
-         @Override
-         public void excluirCliente(String login, String senha) {
-                  if (repository.contem(login)) {
-                           Usuario usuario = repository.buscarPorLogin(login);
+    @Override
+    public void excluirCliente(String login, String senha) {
+        if (repository.contem(login)) {
+            Usuario usuario = repository.buscarPorLogin(login);
 
-                           if (usuario instanceof Cliente cliente) {
-                                    if (cliente.getSenha().equals(senha)) {
-                                             repository.excluir(login);
-                                    }
-                           } else {
-                                    throw new LoginInvalidoException("Usuário não é um cliente.");
-                           }
-                  } throw new LoginInvalidoException("Usuário não existe.");
-         }
+            if (usuario instanceof Cliente) {
+                if (usuario.getSenha().equals(senha)) {
+                    repository.excluir(login);
+                }
+            } else {
+                throw new LoginInvalidoException("Usuário não é um cliente.");
+            }
+        }
+        throw new LoginInvalidoException("Usuário não existe.");
+    }
 }
